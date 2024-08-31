0e3d5eb9b82ed7a685fe640e873b3cadc730cf23
mastachiii <mastachii273@gmail.com>
2024-08-31 16:07:12 +0800

    Stared working on gameboard functionality

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-let board = [ 
-    ['.', '.', '.'], 
-    ['.', '.', '.'], 
-    ['.', '.', '.'] 
-]
+const arr = ['X', '2', '3',
+                   '4', 'X', '6',
+                   '7', '8', 'X'
+                  ]
 
-const makePlayer = function (name, mark){
+function searchArr (arr){
 
-    const markBoard = function (x, y){
+    let index = 0;
 
-        // Checks if board is not occupied by an existing mark.
-        board[x][y] = board[x][y] === '.' ? this.mark : board[x][y];
+    // X - AXIS
+    for (let i = 1; i < 4; i++){
+
+        const currentRow = [arr[index], arr[index + 1], arr[index + 2]].join('')
+
+        if (currentRow === 'XXX') alert('TRUE'); 
+
+        index = 3 * i;
     }
 
-    return { name, mark, markBoard };
+    // Y-AXIS
+    for (let i = 0; i < 3; i++){
+
+        const currentColumn = [arr[0 + i], arr[3 + i], arr[6 + i]].join('')
+
+        if (currentColumn === 'XXX') alert('TRUE');
+    }
+
+    // DIAGONALS
+    const diag1 = [arr[0], arr[4], arr[8]].join('')
+           , diag2 = [arr[2], arr[4], arr[6]].join('');
+
+    if (diag1 === 'XXX') return true;
 }
 
-let player1 = makePlayer('Peter', 'X')
-  , player2 = makePlayer('Clark', 'O');
